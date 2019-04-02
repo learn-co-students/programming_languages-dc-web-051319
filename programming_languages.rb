3a514feed4d6e14fad980c2b99713b479dc2e9c1
Matt Williams <matt.seongjun@gmail.com>
2019-04-02 12:54:03 -0400

    Interate over iterations.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,46 @@
 def reformat_languages(languages)
-  # your code here
+    reform = {}
+    languages.each_value{ |names| reform = reform.merge(names) }
+
+    reform.map{ |name, value_hash| value_hash[:style] = [] }
+
+    reform.each do |name, value_hash|
+        languages.each do |style, name_hash|
+            name_hash.keys.include?(name) ? value_hash[:style] << style : nil
+        end
+    end
+
+    return reform
 end
+
+ puts reformat_languages({
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+})
