c701bed8017db5517b15084d21d46f5b3c2df46b
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-01 03:57:09 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,33 @@
 def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+new_hash = {}
+
+languages.each do |key_top, lang_data|
+  lang_data.each do |key_lang, type_data|
+    if new_hash.keys.include?(key_lang) == false
+       new_hash[key_lang] = {type: type_data[:type], style: [key_top]}
+    else 
+        new_hash[key_lang][:style] << key_top
+    end 
+   end 
 end
+
+return new_hash
+
+end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
